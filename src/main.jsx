7d535a809ee231f596dcc15723ce03d4c55f7bc6
Fux1988 <mart.szewczyk88@gmail.com>
2026-03-02 23:57:33 +0100

    router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import { BrowserRouter } from "react-router-dom";
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from "react-router-dom"
+import './index.css'
+import App from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename="/flower-plant">
-    <App />
-  </BrowserRouter>
-);
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <BrowserRouter basename="/flower-plant">
+      <App />
+    </BrowserRouter>
+  </StrictMode>
+)
