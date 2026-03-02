a3b45b694ce23fc3e66ad33a19bf2af8553f45a0
Fux1988 <mart.szewczyk88@gmail.com>
2026-03-02 23:47:59 +0100

    fix router base

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,10 @@
-// main.jsx
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { HashRouter } from "react-router-dom";
-import './index.css';
-import App from './App.jsx';
+import { BrowserRouter } from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
-  </StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter basename="/flower-plant">
+    <App />
+  </BrowserRouter>
 );
